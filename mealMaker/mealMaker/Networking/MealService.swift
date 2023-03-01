5fd76f0025a57f994127ed8d7020cb053aba95bd
Chase <chaseaohill@gmail.com>
2023-03-01 12:05:41 -0500

    Completed Networking file

--- mealMaker/mealMaker/Networking/MealService.swift
+++ mealMaker/mealMaker/Networking/MealService.swift
@@ -30,13 +30,42 @@ struct MealService {
                 let topLevel = try JSONDecoder().decode(CategoryTopLevelDictionary.self, from: data)
                 completion(.success(topLevel.categories))
             } catch {
-                print("There was an error decoding the data: \(error.localizedDescription)")
+                
+                print("There was an error decoding the category data: \(error.localizedDescription)")
                 completion(.failure(.unableToDecode)) ; return
             }
         } .resume()
     }
     
-    static func fetchMealsInCategory() {
+    static func fetchMealsInCategory(forCategory category: Category, completion: @escaping (Result<[Meal], NetworkError>) -> Void) {
         
+        guard let baseURL = URL(string: Constants.MealService.mealsInCategoryBaseURL) else { completion(.failure(.invalidURL)) ; return }
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let categoryQueryItem = URLQueryItem(name: Constants.MealService.categoryQueryKey, value: category.categoryName)
+        urlComponents?.queryItems = [categoryQueryItem]
+        guard let finalURL = urlComponents?.url else { completion(.failure(.invalidURL)) ; return }
+        print("Meals URL: \(finalURL)")
+        
+        URLSession.shared.dataTask(with: finalURL) { data, response, error in
+            if let error = error {
+                completion(.failure(.thrownError(error))) ; return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Meals Status Code: \(response.statusCode)")
+            }
+            
+            guard let data = data else { completion(.failure(.noData)) ; return }
+            
+            do {
+                
+                let topLevel = try JSONDecoder().decode(MealTopLevelDictionary.self, from: data)
+                completion(.success(topLevel.meals))
+            } catch {
+                
+                print("There was an error decoding the meal data: \(error.localizedDescription)")
+                completion(.failure(.unableToDecode)) ; return
+            }
+        } .resume()
     }
 }
